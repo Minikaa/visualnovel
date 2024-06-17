49734d40b29b32ceb511a5b2c0e442038e10bcb0
Minik <d6k6@protonmail.com>
2024-06-17 16:10:53 +0200

    Reading Text Files

--- VisualNovel/Assets/_TESTING/Scripts/Testing_Architect.cs
+++ VisualNovel/Assets/_TESTING/Scripts/Testing_Architect.cs
@@ -9,6 +9,8 @@ namespace TESTING
         DialogueSystem ds;
         TextArchitect architect;
 
+        public TextArchitect.BuildMethod bm = TextArchitect.BuildMethod.instant;
+
         string[] lines = new string[5]
         {
             "To jest losowa linia dialogu.",
@@ -23,13 +25,22 @@ namespace TESTING
         {
             ds = DialogueSystem.instance;
             architect = new TextArchitect(ds.dialogueContainer.dialogueText);
-            architect.buildMethod = TextArchitect.BuildMethod.typewriter;
+            architect.buildMethod = TextArchitect.BuildMethod.fade;
             architect.speed = 0.5f;
         }
 
         // Update is called once per frame
         void Update()
         {
+            if (bm != architect.buildMethod)
+            {
+                architect.buildMethod = bm;
+                architect.Stop();
+            }
+
+            if (Input.GetKeyDown(KeyCode.S)) //to dotyczy klawisza S , trzeba bedzie to zmienic
+                architect.Stop();
+
             string longLine = "To jest bardzo d³uga linia która nie ma sensu, ale muszê zape³niæ j¹ ró¿n¹ treœci¹, poniewa¿ treœæ jest dobra, dobra, dobra , dobra i lepsza ni¿ lorem ipsum lol toœ coœ musia³am tu wpisaæ, dobra i lepsza ni¿ lorem ipsum lol toœ coœ musia³am tu wpisaæ,dobra i lepsza ni¿ lorem ipsum lol toœ coœ musia³am tu wpisaæ";
             if (Input.GetKeyDown(KeyCode.Space)) //to dotyczy klikniêcia spacji, bêdzie trzeba to wymieniæ na klikniêcie ekranu NA RAZIE ZOSTAWIAM BO TRZEBA JAKOŒ SPRAWDZAÆ NA KOMPIE
             {
@@ -49,6 +60,27 @@ namespace TESTING
                 architect.Append(longLine);
                 //architect.Append(lines[Random.Range(0, lines.Length)]);
             }
+            // dla mobilnych:
+            /*if (Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0);
+                if (touch.phase == TouchPhase.Began)
+                {
+                    string longLine = "To jest bardzo d³uga linia która nie ma sensu, ale muszê zape³niæ j¹ ró¿n¹ treœci¹, poniewa¿ treœæ jest dobra, dobra, dobra , dobra i lepsza ni¿ lorem ipsum lol toœ coœ musia³am tu wpisaæ, dobra i lepsza ni¿ lorem ipsum lol toœ coœ musia³am tu wpisaæ,dobra i lepsza ni¿ lorem ipsum lol toœ coœ musia³am tu wpisaæ";
+                    if (architect.isBuilding)
+                    {
+                        if (!architect.hurryUp)
+                            architect.hurryUp = true;
+                        else
+                            architect.ForceComplete();
+                    }
+                    else
+                    {
+                        architect.Build(longLine);
+                        // architect.Build(lines[Random.Range(0, lines.Length)]);
+                    }
+                }
+            }*/
         }
 
     }
